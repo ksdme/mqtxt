aef8075676763f2cc0c08711054bb27dc43b3484
Kilari Teja <kilariteja9@gmail.com>
2019-04-13 23:41:56 +0530

    Initial Commit

---
+++ app/src/main/java/xyz/ksdme/mqtxt/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.ksdme.mqtxt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
